de517bc0fe8cd64f8c2c324dec59ea87e2adf34d
angel <bo0477@192.168.7.19>
2017-10-03 13:02:03 +0200

    functional testing

---
+++ src/test/java/es/upm/miw/apaw/theme/CompanyResourceFunctionalTesting.java
@@ -0,0 +1,14 @@
+package es.upm.miw.apaw.theme;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompanyResourceFunctionalTesting {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ src/test/java/es/upm/miw/apaw/theme/StaffResourceFunctionalTesting.java
@@ -0,0 +1,14 @@
+package es.upm.miw.apaw.theme;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StaffResourceFunctionalTesting {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
